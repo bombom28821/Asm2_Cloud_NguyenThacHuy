e61e915d47b25a324d5bef87fbe98d8f22cecfaa
bombom28821 <bombom28821@gmail.com>
2021-07-06 20:11:16 +0700

    new update

--- app.js
+++ app.js
@@ -46,12 +46,12 @@ app.post('/loginAccount', async (req, res) => {
         res.render('login', { accountError: 'Name must have @ character', nameAccount: req.session.nameAccount, pass: req.session.passAccount })
     } else if (passInput.trim().length < 4) {
         res.render('login', { passError: 'Password is more than 3 character', nameAccount: req.session.nameAccount, pass: req.session.passAccount })
-    }else if (found) {
+    } else if (found) {
         req.session.username = nameInput;
         const results = await dbHandler.searchProduct('', 'Products');
         res.render('manage', { model: results });
     } else {
-        res.render('login', {errorMsg: "Login failed!", nameAccount: req.session.nameAccount, pass: req.session.passAccount });
+        res.render('login', { errorMsg: "Login failed!", nameAccount: req.session.nameAccount, pass: req.session.passAccount });
     }
 })
 
@@ -62,9 +62,9 @@ app.post('/search', async (req, res) => {
 })
 
 app.get('/addProduct', (req, res) => {
-    if(req.session.username != null){
+    if (req.session.username != null) {
         res.render('addProduct');
-    }else{
+    } else {
         res.render('login');
     }
 })
@@ -77,11 +77,15 @@ app.post('/doAddProduct', async (req, res) => {
     req.session.priceProduct = priceInput;
     req.session.descProduct = descInput;
     if (nameInput.startsWith('p') == true || nameInput.startsWith('t') == true) {
-        res.render('addProduct', { nameError: 'Name has length >= 3',nameProduct: req.session.nameProduct,
-         priceProduct: req.session.priceProduct, descProduct: req.session.descProduct});
+        res.render('addProduct', {
+            nameError: 'Name has length >= 3', nameProduct: req.session.nameProduct,
+            priceProduct: req.session.priceProduct, descProduct: req.session.descProduct
+        });
     } else if (isNaN(priceInput) || priceInput.trim().length == 0 || priceInput > 1000 || priceInput <= 0) {
-        res.render('addProduct', { priceError: 'Price is number and has price <= 1000 USD',nameProduct: req.session.nameProduct,
-         priceProduct: req.session.priceProduct, descProduct: req.session.descProduct });
+        res.render('addProduct', {
+            priceError: 'Price is number and has price <= 1000 USD', nameProduct: req.session.nameProduct,
+            priceProduct: req.session.priceProduct, descProduct: req.session.descProduct
+        });
     } else {
         var newProduct = { name: nameInput, price: priceInput, description: descInput };
         await dbHandler.insertOneIntoCollection(newProduct, 'Products');
@@ -90,12 +94,12 @@ app.post('/doAddProduct', async (req, res) => {
 })
 
 app.get('/edit', async (req, res) => {
-    if(req.session.username != null){
+    if (req.session.username != null) {
         const id = req.query.id;
 
         const productToEdit = await dbHandler.findById(id);
         res.render('edit', { product: productToEdit })
-    }else{
+    } else {
         res.render('login');
     }
 })
@@ -107,9 +111,9 @@ app.post('/update', async (req, res) => {
     const descInput = req.body.txtDescription;
     const results = await dbHandler.findById(id);
     if (nameInput.trim().length < 3) {
-        res.render('edit', {product: results, nameError: 'Name has length >= 3' });
+        res.render('edit', { product: results, nameError: 'Name has length >= 3' });
     } else if (isNaN(priceInput) || priceInput.trim().length == 0 || priceInput > 1000 || priceInput <= 0) {
-        res.render('edit', {product: results, priceError: 'Price is number and has price <= 1000 USD' });
+        res.render('edit', { product: results, priceError: 'Price is number and has price <= 1000 USD' });
     } else {
         const newValues = { $set: { name: nameInput, price: priceInput, description: descInput } };
         await dbHandler.updateProduct(newValues, id);
@@ -118,22 +122,67 @@ app.post('/update', async (req, res) => {
 })
 
 app.get('/delete', async (req, res) => {
-    if(req.session.username != null){
+    if (req.session.username != null) {
         const id = req.query.id;
         await dbHandler.deleteProduct(id);
         res.redirect('manage');
-    }else{
+    } else {
         res.render('login');
     }
 
 })
 
 app.get('/logout', (req, res) => {
-    if(req.session.username != null){
+    if (req.session.username != null) {
         req.session.destroy();
         res.render('');
-    }else{
+    } else {
         res.render('login');
+    }
+})
+
+// SORT 
+app.post('/sort', async (req, res) => {
+    let select = req.body.select;
+    if (select == 'priceUp') {
+        let results = await dbHandler.searchProduct('', 'Products');
+        let temps = results;
+        for (let i = 0; i < temps.length; i++) {
+            for (let j = 0; j < temps.length; j++) {
+                if (temps[i].price < temps[j].price) {
+                    let temp1 = temps[j];
+                    temps[j] = temps[i];
+                    temps[i] = temp1;
+                }
+            }
+        }
+        res.render('manage', { model: temps });
+    } else if (select == 'priceDown') {
+        let results = await dbHandler.searchProduct('', 'Products');
+        let temps = results;
+        for (let i = 0; i < temps.length; i++) {
+            for (let j = 0; j < temps.length; j++) {
+                if (temps[i].price > temps[j].price) {
+                    let temp1 = temps[j];
+                    temps[j] = temps[i];
+                    temps[i] = temp1;
+                }
+            }
+        }
+        res.render('manage', { model: temps });
+    } else if (select == 'name') {
+        let results = await dbHandler.searchProduct('', 'Products');
+        let temps = results;
+        for (let i = 0; i < temps.length; i++) {
+            for (let j = 0; j < temps.length; j++) {
+                if (temps[i].name.toLowerCase() < temps[j].name.toLowerCase()) {
+                    let temp1 = temps[j];
+                    temps[j] = temps[i];
+                    temps[i] = temp1;
+                }
+            }
+        }
+        res.render('manage', { model: temps });
     }
 })
 
